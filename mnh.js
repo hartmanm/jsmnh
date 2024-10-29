74beacd8550a5ffddad26e9a01273f30b20c22f0
Michael Neill Hartman <16545449+hartmanm@users.noreply.github.com>
2024-10-29 17:08:57 +0000

    rename functions, allow for processing raw

--- mnh.js
+++ mnh.js
@@ -3,7 +3,7 @@
 // mnh_license@proton.me
 // https://github.com/hartmanm
 
-const loadAndProcessData = async (url) => {
+const f_process_remote_data = async (url) => {
 const theMap = {};
 let lineNumber = 1;
 try {
@@ -13,20 +13,22 @@ throw new Error('response.statusText: ' + response.statusText);
 }
 const contentType = response.headers.get('Content-Type');
 const textData = await response.text();
+
 if (contentType.includes('application/json')) {
 const data = JSON.parse(textData);
 if (Array.isArray(data)) {
-data.forEach(line => processLine(line, theMap, lineNumber++));
+data.forEach(line => f_process_line(line, theMap, lineNumber++));
 }
 } else if (contentType.includes('text/csv') || contentType.includes('text/plain')) {
 const lines = textData.split('\n');
 lines.forEach(line => {
 if (line) {
-processLine(line, theMap, lineNumber++);
+f_process_line(line, theMap, lineNumber++);
 }
 });
 } else {
-throw new Error('content type not implemented: ' + contentType);
+console.warn('unknown content type:',contentType);
+console.log('first 100 received: !',textData.substring(0, 100),'!');
 }
 return theMap;
 } catch (error) {
@@ -35,7 +37,7 @@ throw error;
 }
 };
 
-const processLine = (line, theMap, lineNumber, ifs = ',') => {
+const f_process_line = (line, theMap, lineNumber, ifs = ',') => {
 const tokens = line.split(ifs);
 if (!theMap[lineNumber]) {
 theMap[lineNumber] = [];
