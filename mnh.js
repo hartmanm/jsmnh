5a2ddc05e209d2dea305ac0212998f29b65bc4a8
Michael Neill Hartman <16545449+hartmanm@users.noreply.github.com>
2024-10-29 17:23:01 -0700

    added parser components

--- mnh.js
+++ mnh.js
@@ -20,7 +20,47 @@ throw error;
 }
 };
 
-const f_process_line = (line,the_map,line_number,ifs) => {
+const f_split_with_ifs = (input_segment,result_map,segment_number,ifs) => {
+const tokens = input_segment.split(ifs);
+if(!result_map[segment_number]){
+result_map[segment_number]=[];
+}
+tokens.forEach(item => {
+result_map[segment_number].push(item);
+});
+};
+
+const f_process_kv = (input_segment,kv_map) => {
+let keys=[];
+let values=[];
+f_split_segment(keys,values,input_segment,':');
+let segment_number=0;
+keys.forEach(item => {
+if(!values[segment_number]){
+values[segment_number]=[];
+console.log("!values[segment_number]",segment_number);
+}
+kv_map[keys[segment_number]].push(values[segment_number]);
+segment_number++;
+});
+};
+
+function f_split_segment(lefths,righths,input_segment,split_on_char){
+let parts =[]
+if(typeof input_segment === 'string'){
+parts = input_segment.split(split_on_char);
+} else {
+console.log("if(typeof input_segment !== 'string'){");
+}
+if(parts.length<2){
+lefths  =[];
+righths =[];
+}
+lefths = parts.slice(0,-1);
+righths = parts.slice(-1);
+}
+
+const dep_f_process_line = (line,the_map,line_number,ifs) => {
 const tokens = line.split(ifs);
 if(!the_map[line_number]){
 the_map[line_number] = [];
